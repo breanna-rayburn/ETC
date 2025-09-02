93bea038741e71f1ad620449a4758b2a046dd357
Breanna Rayburn <breanna.rayburn@proton.me>
2025-09-02 09:35:08 -0700

    setup done

---
+++ CIT28/ps0/cyberchase/a-q.sql
@@ -0,0 +1,19 @@
+-- starting the pow workflow
+-- changes terminal output for readability 
+.mode box
+  
+-- changes terminal output from standard output (stdout) to pow.txt file
+.output '| cat >> pow-a.txt'
+  
+-- updates the pow.txt with content in single quotes
+.print 'first query'  
+  
+-- SQL command (query) to view all the column contents in the longlist table   
+-- Copy your SQL command below here
+
+-- After each commit comment out the queries written and then continue to write your queries in this part of the file
+
+-- changes back to stdout    
+.output stdout  
+
+-- bash command sqlite3 longlist.db < p2-q.sql
